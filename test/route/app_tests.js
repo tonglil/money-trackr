703b656c37bc5220fa7ed5b0cfa99958992be4da
scolin22 <colin@bunnylaundering.com>
2014-04-02 02:24:42 -0700

    mods

--- test/route/app_tests.js
+++ test/route/app_tests.js
@@ -1,16 +1,14 @@
 var request = require('../../app/node_modules/supertest');
 var express = require('../../app/node_modules/express');
 var app = express();
-var nock = require('../../app/node_modules/nock');
-
 
 describe('index route', function () {
-
 
   it('should respond with a 200 with no query parameters', function (done) {
 
     request(app)
       .get('/user')
+      .expect('Content-Type', /html/)
       .expect(200, done);
 
   });
