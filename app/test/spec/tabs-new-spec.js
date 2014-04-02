b4caa4a0ead73b81a2d402fae8c2383486bf5ba9
scolin22 <colin@bunnylaundering.com>
2014-04-02 16:11:37 -0700

    fix bugs in mocha unit tests

--- app/test/spec/tabs-new-spec.js
+++ app/test/spec/tabs-new-spec.js
@@ -3,7 +3,7 @@ var tabsnew = require("../../src/routes/tabs-new");
 describe("convert number to currency", function () {
   it("it should make 2 decimal points", function () {
     console.log(tabsnew);
-    // var result = tabsnew.makeCurrency(2);
-    // expect(result).toBe(2.00);
+    var result = tabsnew.makeCurrency(2);
+    expect(result).toBe(2.00);
   });
 });
